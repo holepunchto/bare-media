d63db3d1d506f345c55c1782a86c7a699031e864
Eduard Castellano <educastellano08@gmail.com>
2025-07-11 11:42:55 +0200

    Pear.config.link -> Pear.config.applink

--- client/cross-spawn/pear.js
+++ client/cross-spawn/pear.js
@@ -2,6 +2,6 @@ import path from 'path'
 
 export function spawn (sourcePath) {
   const normalized = path.normalize(sourcePath)
-  const link = Pear.key ? `${Pear.config.link}/${normalized}` : path.join(Pear.config.dir, ...normalized.split('/'))
+  const link = Pear.key ? `${Pear.config.applink}/${normalized}` : path.join(Pear.config.dir, ...normalized.split('/'))
   return Pear.worker.run(link)
 }
