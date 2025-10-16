e2663e88379e032431af245e97cf3261173a20b9
Eduard Castellano <educastellano08@gmail.com>
2025-10-16 16:01:33 +0200

    try inc port

--- test/helpers.js
+++ test/helpers.js
@@ -5,6 +5,8 @@ import BlobServer from 'hypercore-blob-server'
 import Hyperblobs from 'hyperblobs'
 import tmp from 'test-tmp'
 
+let port
+
 export async function makeHttpLink(t, path, mimetype) {
   const store = new Corestore(await tmp())
 
@@ -17,16 +19,17 @@ export async function makeHttpLink(t, path, mimetype) {
   // save file
   const id = await blobs.put(buffer)
 
-  const server = new BlobServer(store)
+  const server = new BlobServer(store, { port })
+  port = server.port + 1
   t.teardown(() => server.close())
   await server.listen()
 
   // get link
-  console.log('makeHttpLink 0', blobs.core.key, id)
+  console.log('makeHttpLink 0')
   const httpLink = server.getLink(blobs.core.key, { blob: id })
   console.log('makeHttpLink 1', httpLink)
   const res = await fetch(httpLink)
-  console.log('makeHttpLink 2', res)
+  console.log('makeHttpLink 2')
   t.is(res.status, 200)
   console.log('makeHttpLink 3')
   t.alike(await res.buffer(), buffer)
