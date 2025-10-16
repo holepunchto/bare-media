d14a6ed87f47fd48849d4f2040503b81a4d670ce
Eduard Castellano <educastellano08@gmail.com>
2025-10-16 15:48:47 +0200

    more prints

--- test/helpers.js
+++ test/helpers.js
@@ -6,32 +6,31 @@ import Hyperblobs from 'hyperblobs'
 import tmp from 'test-tmp'
 
 export async function makeHttpLink(t, path, mimetype) {
-  console.log('makeHttpLink 0')
   const store = new Corestore(await tmp())
 
-  console.log('makeHttpLink 1')
   const core = store.get({ name: 'test' })
   const blobs = new Hyperblobs(core)
   t.teardown(() => blobs.close())
-  console.log('makeHttpLink 2')
 
   const buffer = fs.readFileSync(path)
 
   // save file
   const id = await blobs.put(buffer)
-  console.log('makeHttpLink 3')
 
   const server = new BlobServer(store)
   t.teardown(() => server.close())
   await server.listen()
-  console.log('makeHttpLink 4')
 
   // get link
+  console.log('makeHttpLink 0', blobs.core.key, id)
   const httpLink = server.getLink(blobs.core.key, { blob: id })
+  console.log('makeHttpLink 1', httpLink)
   const res = await fetch(httpLink)
+  console.log('makeHttpLink 2', res)
   t.is(res.status, 200)
+  console.log('makeHttpLink 3')
   t.alike(await res.buffer(), buffer)
-  console.log('makeHttpLink 5')
+  console.log('makeHttpLink 4')
 
   return httpLink
 }
