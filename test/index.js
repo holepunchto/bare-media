2f8c806107fff00ae97cf0bba2c450b558cfc710
Tony Gorez <gorez.tony@gmail.com>
2026-01-19 15:21:34 +0100

    test: mkv to mp4
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/index.js
+++ test/index.js
@@ -652,6 +652,41 @@ test('media.transcode() - mp4 to webm (unit test)', async (t) => {
   t.is(totalOutputBuffer[3], 0xa3, 'Output starts with EBML header byte 3')
 })
 
+test('media.transcode() - mkv to mp4 (unit test)', async (t) => {
+  const path = './test/fixtures/sample.mkv'
+  const outputParameters = {
+    format: 'mp4',
+    width: 320,
+    height: 240
+  }
+
+  const mockStream = {
+    data: {
+      path,
+      outputParameters
+    },
+    chunks: [],
+    write(chunk) {
+      this.chunks.push(chunk)
+    },
+    end() {
+      this.ended = true
+    }
+  }
+
+  await media.transcode(mockStream)
+
+  t.ok(mockStream.chunks.length > 0, 'Received some chunks')
+  t.ok(mockStream.ended, 'Stream was ended')
+
+  const totalOutputBuffer = b4a.concat(mockStream.chunks.map((c) => c.buffer))
+  t.ok(totalOutputBuffer.length > 0, 'Total output buffer has data')
+
+  // Check for MP4 header (ftyp box)
+  const header = b4a.toString(totalOutputBuffer.subarray(4, 8))
+  t.is(header, 'ftyp', 'Output starts with MP4 ftyp marker')
+})
+
 // Video preview tests
 
 test('media.createVideoPreview() single frame of .mp4', async (t) => {
