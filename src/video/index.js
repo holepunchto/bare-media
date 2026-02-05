b8e7b334f0518a9105630c94695341a24bdb28a9
Eduard Castellano <educastellano08@gmail.com>
2026-02-05 12:00:48 -0600

    Ensure close file in extractFrames (#34)

--- src/video/index.js
+++ src/video/index.js
@@ -109,9 +109,16 @@ class VideoPipeline {
   }
 
   extractFrames(opts) {
-    const fd = fs.openSync(this.input, 'r')
-    const result = extractFrames(fd, opts)
-    fs.closeSync(fd)
+    let fd
+    let result
+
+    try {
+      fd = fs.openSync(this.input, 'r')
+      result = extractFrames(fd, opts)
+    } finally {
+      fs.closeSync(fd)
+    }
+
     return result
   }
 
